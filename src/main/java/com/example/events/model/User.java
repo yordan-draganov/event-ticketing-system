490920a2fb77ac715ae314378f9d755dc75d0508
yordan-draganov <yordan.draganov.2211@gmail.com>
2025-10-26 19:04:48 +0200

    made name unique and added some annotations

--- src/main/java/com/example/events/model/User.java
+++ src/main/java/com/example/events/model/User.java
@@ -19,28 +19,37 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(columnDefinition = "UUID")
+    @Getter
     private UUID id;
 
-    @Column(nullable = false, unique = true, length = 255)
+    @NonNull
+    @Column(nullable = false, length = 255)
+    @Getter @Setter
     private String email;
 
+    @NonNull
     @Column(nullable = false, length = 255)
+    @Getter @Setter
     private String password;
 
-    @Column(nullable = false, length = 255)
+    @NonNull
+    @Column(nullable = false, unique = true, length = 255)
+    @Getter @Setter
     private String name;
 
+    @NonNull
     @Enumerated(EnumType.STRING)
     @Column(name = "role")
     @org.hibernate.annotations.JdbcTypeCode(org.hibernate.type.SqlTypes.NAMED_ENUM)
+    @Getter @Setter
     private UserRole role;
 
+
+    @Column(name = "created_at", updatable = false, nullable = false)
     @CreationTimestamp
-    @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
 
+    @Column(name = "updated_at", nullable = false)
     @UpdateTimestamp
-    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
-
 }
