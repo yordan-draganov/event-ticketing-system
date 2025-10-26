ea9431961d75c7c853476e4254695ce7f64e60ff
yordan-draganov <yordan.draganov.2211@gmail.com>
2025-10-26 19:17:46 +0200

    add name based query method in repository

--- src/main/java/com/example/events/repository/UserRepository.java
+++ src/main/java/com/example/events/repository/UserRepository.java
@@ -9,6 +9,9 @@ import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
-    Optional<User> findByEmail(String email);
-    boolean existsByEmail(String email);
+    Optional<User> findByName(String name);
+    boolean existsByName(String name);
+
+//    Optional<User> findByEmail(String email);
+//    boolean existsByEmail(String email);
 }
