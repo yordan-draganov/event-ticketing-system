9dfbd82c1559b1ddab307905545098de142aa260
yordan-draganov <yordan.draganov.2211@gmail.com>
2025-11-04 20:18:41 +0200

    Created jwt request and response dtos

---
+++ src/main/java/com/example/events/DTO/LoginRequest.java
@@ -0,0 +1,13 @@
+package com.example.events.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequest {
+    private String name;
+    private String password;
+}
